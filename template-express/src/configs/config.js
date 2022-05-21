ba69131e39af70d94071d0e80f768b5da8508f92
MrYasuo <buiduchai0706vn@gmail.com>
2022-05-21 11:06:14 +0700

    update

--- template-express/src/configs/config.js
+++ template-express/src/configs/config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const path = require("path");
 
-dotenv.config({ path: path.join(__dirname, "../../.env") });
+dotenv.config({ path: path.join(__dirname, "../../.env.example") });
 
 const config = {
 	BASE: {
